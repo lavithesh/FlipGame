3b4840966128390260878a436050b68a81e0364e
Lavithesh <lavithesh7@gmail.com>
2025-04-28 12:42:54 +0530

    gamelogicforFLipped added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useMemo, useRef, useState } from "react";
+import Confetti from "react-confetti";
 import "./App.css";
 
 function App() {
@@ -19,6 +18,13 @@ function App() {
     "🍕",
   ];
   const [pices, setPices] = useState([]);
+  const timeout = useRef();
+  const isGameCompleted = useMemo(() => {
+    if (pices.length > 0 && pices.every((pice) => pice.solved)) {
+      return true;
+    }
+    return false;
+  }, [pices]);
 
   const start = () => {
     const duplicateArray = [...icons, ...icons];
@@ -30,7 +36,7 @@ function App() {
         emoji: duplicateArray[newIndex],
         flipped: false,
         solved: false,
-        position: newIndex.length,
+        position: newArray.length,
       });
       duplicateArray.splice(newIndex, 1);
     }
@@ -39,14 +45,70 @@ function App() {
   useEffect(() => {
     start();
   }, []);
+  const handelActive = (data) => {
+    const flippedData = pices.filter((data) => data.flipped && !data.solved);
+    if (flippedData.length === 2) return;
 
+    const newPices = pices.map((pice) => {
+      if (pice.position === data.position) {
+        pice.flipped = !pice.flipped;
+      }
+      return pice;
+    });
+    setPices(newPices);
+  };
+
+  const gameLogicForFlipped = () => {
+    const flippedData = pices.filter((data) => data.flipped && !data.solved);
+    if (flippedData.length === 2) {
+      timeout.current = setTimeout(() => {
+        if (flippedData[0].emoji === flippedData[1].emoji) {
+          setPices(
+            pices.map((pice) => {
+              if (
+                pice.position === flippedData[0].position ||
+                pice.position === flippedData[1].position
+              ) {
+                pice.solved = true;
+              }
+              return pice;
+            })
+          );
+        } else {
+          setPices(
+            pices.map((pice) => {
+              if (
+                pice.position === flippedData[0].position ||
+                pice.position === flippedData[1].position
+              ) {
+                pice.flipped = false;
+              }
+              return pice;
+            })
+          );
+        }
+      }, 800);
+    }
+  };
+  useEffect(() => {
+    gameLogicForFlipped();
+    return () => {
+      clearTimeout(timeout.current);
+    };
+  }, [pices]);
   return (
     <>
       <h1>Welcome to Flip Game</h1>
       <div className="container">
         {pices.map((data, index) => {
           return (
-            <div className="flip-card" key={index}>
+            <div
+              className={`flip-card ${
+                data.flipped || data.solved ? "active" : ""
+              }`}
+              key={index}
+              onClick={() => handelActive(data)}
+            >
               <div className="flip-card-inner">
                 <div className="flip-card-front"></div>
                 <div className="flip-card-back">{data.emoji}</div>
@@ -55,6 +117,12 @@ function App() {
           );
         })}
       </div>
+      {isGameCompleted && (
+        <div className="game-completed">
+          <h1>YOU WIN!!!</h1>{" "}
+          <Confetti width={window.innerWidth} height={window.innerHeight} />
+        </div>
+      )}
     </>
   );
 }
